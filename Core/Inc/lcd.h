69dd92f9785fc1b063dc03cabd0a067c87448762
rubin-khadka <rubin.robotic@gmail.com>
2025-11-26 14:43:07 +0100

    added lcd initial files

---
+++ Core/Inc/lcd.h
@@ -0,0 +1,29 @@
+/* LCD Header file */
+
+#ifndef LCD_H
+#define LCD_H
+
+#include "main.h"
+#include "timer.h"
+
+// LCD Pin Macros
+#define RS(x) (x==1 ? (HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_SET)) : (HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, GPIO_PIN_RESET)))
+#define RW(x) (x==1 ? (HAL_GPIO_WritePin(RW_GPIO_Port, RW_Pin, GPIO_PIN_SET)) : (HAL_GPIO_WritePin(RW_GPIO_Port, RW_Pin, GPIO_PIN_RESET)))
+#define EN(x) (x==1 ? (HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_SET)) : (HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_RESET)))
+#define D4(x) (x==1 ? (HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, GPIO_PIN_SET)) : (HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, GPIO_PIN_RESET)))
+#define D5(x) (x==1 ? (HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, GPIO_PIN_SET)) : (HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, GPIO_PIN_RESET)))
+#define D6(x) (x==1 ? (HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, GPIO_PIN_SET)) : (HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, GPIO_PIN_RESET)))
+#define D7(x) (x==1 ? (HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, GPIO_PIN_SET)) : (HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, GPIO_PIN_RESET)))
+
+// LCD Macros
+
+// LCD Function prototypes
+void lcd_init(void);
+void lcd_write_command(uint8_t param);
+void lcd_write_data(uint8_t param);
+void lcd_process_4bit(uint8_t data);
+void lcd_set_cursor(uint8_t row, uint8_t col);
+void lcd_print(const char *);
+void lcd_clear(void);
+
+#endif
---
+++ Core/Src/lcd.c
@@ -0,0 +1,88 @@
+/* LCD Source file */
+
+#include "lcd.h"
+
+void lcd_init(void)
+{
+	// Set control pins for commmand mode
+	RS(0); // Command mode 
+	RW(0); // Write mode
+	
+	// Wait for LCD power stabilization
+	// Manual initialisation sequence
+	HAL_Delay(50);
+	lcd_process_4bit(0x03);
+	HAL_Delay(5);           
+	lcd_process_4bit(0x03);  
+	HAL_Delay(1);           
+	lcd_process_4bit(0x03);  
+	HAL_Delay(1);      
+	
+	// Switch to 4 bit mode
+	lcd_process_4bit(0x02);  // Send 0010 (Switch to 4-bit mode)
+	HAL_Delay(1);  
+	
+	lcd_write_command(0x28); // 4-bit, 2-line, 5x8
+	lcd_write_command(0x0C); // Display ON
+	lcd_write_command(0x01); // Clear display
+	HAL_Delay(2);
+	lcd_write_command(0x06); // Entry mode
+}
+
+void lcd_write_command(uint8_t param)
+{
+	// do busy check here
+	RS(0);
+	RW(0);
+	
+	lcd_process_4bit(param >>4); // High nibble first
+	lcd_process_4bit(param & 0xF); // Low nibble second
+	
+	timer2_lcd_delay_ms(2); // small delay, will replace by timer later
+}
+
+void lcd_write_data(uint8_t param)
+{
+	// do busy check here
+	timer2_lcd_delay_ms(5);
+	RS(1);
+	RW(0);
+	
+	lcd_process_4bit(param >>4); // High nibble first
+	lcd_process_4bit(param & 0xF); // Low nibble second
+	
+	timer2_lcd_delay_ms(5); // small delay, will replace by timer later
+}
+
+void lcd_process_4bit(uint8_t data)
+{
+	// Set data pins using direct bit operation
+	// shifted to get 1/0 value for proper macro behaviour
+	D4((data & 0x01) >> 0); 
+	D5((data & 0x02) >> 1);
+	D6((data & 0x04) >> 2);
+	D7((data & 0x08) >> 3);
+	
+	// Create small pulse, LCD samples data on the falling edge of EN
+	EN(1);
+	timer2_lcd_delay_ms(2); // wait for signal to stablize
+	EN(0); // data get latched
+}
+
+void lcd_set_cursor(uint8_t row, uint8_t col)
+{
+	lcd_write_command((row & 0x01) ? (0xC0 | (col & 0x0F)) : (0x80 | (col & 0x0F)));
+}
+
+void lcd_print(const char *str) 
+{
+	while(*str) 
+	{
+		lcd_write_data(*str++);
+	}
+}
+
+void lcd_clear(void)
+{
+	lcd_write_command(0x01);
+}
